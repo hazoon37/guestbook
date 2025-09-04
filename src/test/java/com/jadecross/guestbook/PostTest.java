3a43c7a48b8328c0f14e44eb17ec9875a54c80df
justin <justin@jadecross.com>
2025-09-04 07:45:12 +0000

    [ADD] TestCase - PostTest.java

--- src/test/java/com/jadecross/guestbook/PostTest.java
+++ src/test/java/com/jadecross/guestbook/PostTest.java
@@ -1,6 +1,6 @@
 package com.jadecross.guestbook;
 
-import staticorg.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class PostTest {
